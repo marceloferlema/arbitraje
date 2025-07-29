1b9d10851d933826cc27447c931c525d581a2da1
Marcelo Lema <marceloferlema@gmail.com>
2025-07-29 01:12:29 -0300

    tiempo

--- arbitraje.py
+++ arbitraje.py
@@ -18,7 +18,7 @@ MERCADO = "bcba"
 TICKERS = os.getenv("TICKERS", "")
 TICKERS = TICKERS.split(",") if TICKERS else []
 
-UMBRAL_VARIACION = 1.2  # En porcentaje
+UMBRAL_VARIACION = 2  # En porcentaje
 INTERVALO_MINUTOS = 1
 
 access_token = None
