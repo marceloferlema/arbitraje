1a166ec2e744ccb89c5e3e575d602e1dcb89380d
marceloferlema <marcelolema@gmail.com>
2025-07-31 11:49:50 -0300

    Update arbitraje.py

--- arbitraje.py
+++ arbitraje.py
@@ -18,7 +18,7 @@ MERCADO = "bcba"
 TICKERS = os.getenv("TICKERS", "")
 TICKERS = TICKERS.split(",") if TICKERS else []
 
-UMBRAL_VARIACION = 3  # En porcentaje
+UMBRAL_VARIACION = 2  # En porcentaje
 INTERVALO_MINUTOS = 1
 
 access_token = None
